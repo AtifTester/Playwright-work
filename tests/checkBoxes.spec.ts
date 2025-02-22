a14fba9e2c92349b0ad1de93c97901738cd9995c
Atif <atestsha@gmail.com>
2025-02-22 18:11:40 +0000

    Refactor and fixed Assertions

--- tests/checkBoxes.spec.ts
+++ tests/checkBoxes.spec.ts
@@ -8,26 +8,24 @@ test('Test Case 1: Validate selected specialties', async ({page}) => {
   await expect(page.locator('.title')).toHaveText('Welcome to Petclinic')
   await page.getByText('Veterinarians').click()
   await page.getByText('all').click()
-  expect(page.getByRole('heading', {name: 'Veterinarians'}))
+  await expect(page.getByRole('heading')).toHaveText('Veterinarians')
 
   await page.getByRole('button', {name: "Edit Vet"}).nth(1).click()
   await expect(page.locator('.selected-specialties')).toHaveText('radiology')
 
   await page.locator('.dropdown-display').click()
 
-  const dropDownDisplay = page.locator('.dropdown-content')
-
   //Validate checkboxes
-  expect(await dropDownDisplay.getByRole('checkbox', {name: 'radiology'}).isChecked()).toBeTruthy()
-  expect(await dropDownDisplay.getByRole('checkbox', {name: 'surgery'}).isChecked()).toBeFalsy()
-  expect(await dropDownDisplay.getByRole('checkbox', {name: 'dentistry'}).isChecked()).toBeFalsy()
+  expect(await page.getByRole('checkbox', {name: 'radiology'}).isChecked()).toBeTruthy()
+  expect(await page.getByRole('checkbox', {name: 'surgery'}).isChecked()).toBeFalsy()
+  expect(await page.getByRole('checkbox', {name: 'dentistry'}).isChecked()).toBeFalsy()
 
   //select and unselect boxes
-  await dropDownDisplay.getByRole('checkbox', {name: 'radiology'}).uncheck()
-  await dropDownDisplay.getByRole('checkbox', {name: 'surgery'}).check()
+  await page.getByRole('checkbox', {name: 'radiology'}).uncheck()
+  await page.getByRole('checkbox', {name: 'surgery'}).check()
   await expect(page.locator('.selected-specialties')).toHaveText('surgery')
 
-  await dropDownDisplay.getByRole('checkbox', {name: 'dentistry'}).check()
+  await page.getByRole('checkbox', {name: 'dentistry'}).check()
  
   await expect(page.locator('.selected-specialties')).toHaveText(['surgery, dentistry'])
 });
